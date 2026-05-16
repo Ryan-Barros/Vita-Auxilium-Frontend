021ea689c8ac987072af9b19ce3cb0cb5bad5d73
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-16 15:48:05 -0300

    Atualiza para inserção de AppRoutes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import RegisterPage from "../pages/RegisterPage";
-import LoginPage from "../pages/LoginPage";
+import AppRoutes from "./AppRoutes";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/auth/register" element={<RegisterPage />} />
-    </Routes>
-  );
+  return <AppRoutes />;
 }

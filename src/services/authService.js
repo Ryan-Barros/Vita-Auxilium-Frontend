657b1ca7d85c8eaa26363962f34e872579de051f
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-17 00:31:15 -0300

    corrige retorno de dados

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,10 +2,10 @@ import api from "./api";
 
 export async function login(email, password) {
   const res = await api.post("/auth/login", { email, password });
-  return res.data;
+  return res;
 }
 
 export async function register(data) {
   const res = await api.post("/auth/register", data);
-  return res.data;
+  return res;
 }
